968422428291c20d1cb6790e42184e4c5512758d
Mikaila Gossman <mikailg@g.clemson.edu>
2023-04-19 21:27:14 -0400

    model building

--- llearn.py
+++ llearn.py
@@ -12,24 +12,26 @@ import cv2 as cv
 import numpy as np
 
 
-class MLP(nn.Module):
-    def __init__(self, inputs, num_layers, outputs):
-        super(MLP, self).__init__()
-        self.num_layers = num_layers
-        self.fc1 = nn.Linear(8192, 256)
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128,64)
-        self.fc4 = nn.Linear(64,32)
-        self.fc5 = nn.Linear(32,10)
-        self.mlp = nn.MLP()
+# class MLP(nn.Module):
+#     def __init__(self, inputs, num_layers, outputs):
+#         super(MLP, self).__init__()
+#         self.num_layers = num_layers
+#         self.fc1 = nn.Linear(8192, 256)
+#         self.fc2 = nn.Linear(256, 128)
+#         self.fc3 = nn.Linear(128,64)
+#         self.fc4 = nn.Linear(64,32)
+#         self.fc5 = nn.Linear(32,10)
+#         self.mlp = nn.MLP()
     
-    def forward(self):
-        
-
+#     def forward(self):
 
 def main():
-
-    #Prepare the training dataset
+   
+    #read in the dataset
+    dataset = pd.read_csv("/home/mikailg/ECE8650/optimizing_checkpoint_restart_project/training_data/combined_training_data.csv")
+    sns.countplot(x = 'nfiles', data=dataset)
+    dataset['strategy'] = dataset['strategy'].astype(category)
+    print(dataset)
 
 
 if __name__ == '__main__':
